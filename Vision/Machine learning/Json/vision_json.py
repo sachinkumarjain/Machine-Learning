757b89f722caec51b3aa8837baf23532195bfca9
Sandeep Kumar <sandeepkumaramni@gmail.com>
2017-06-13 21:29:31 +0530

    Documentation edit

--- Vision/Machine learning/Json/vision_json.py
+++ Vision/Machine learning/Json/vision_json.py
@@ -1,7 +1,7 @@
 import json
 
 def open_json():
-    with open('vision.json') as json_data:
+    with open('vision_google_cloud.json') as json_data:
         data=json.load(json_data)
     #print(data['faceAnnotations'])
     for r in data['faceAnnotations']:
